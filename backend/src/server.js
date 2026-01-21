72325399420718c659657b3824ab9a849f4e7020
Yashvardhan-111 <yash2004ips@gmail.com>
2026-01-21 21:17:00 +0530

    Log in server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
+import { log } from "console";
 
 const app=express();
 
 app.get("/h", (req, res) => res.send("OK"));
 
 const __dirname = path.resolve();
+console.log(__dirname);
 
 // make app ready for deployment
 if (ENV.NODE_ENV === "production") {
