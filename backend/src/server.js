338b47e989c1f3968eab25ca39ec88188dad18bf
Yashvardhan-111 <yash2004ips@gmail.com>
2026-01-28 17:43:25 +0530

    server side auth

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,9 @@ import path from "path";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import { inngest, functions } from "./lib/inngest.js";
+import chatRoutes from "./routes/chatRoutes.js";
 import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
 
 const app=express();
 
@@ -16,7 +18,10 @@ const __dirname = path.resolve();
 app.use(express.json());
 // credentials:true => server allows a browser to include cookies on request
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware());
+
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
 
 // make app ready for deployment
 if (ENV.NODE_ENV === "production") {
