adb7f39b6c54251e2c85518f9c80e2616fa45eba
Yashvardhan-111 <yash2004ips@gmail.com>
2026-02-01 23:18:38 +0530

    Problems Page added

---
+++ frontend/src/lib/utils.js
@@ -0,0 +1,13 @@
+//Returns a DaisyUI badge class based on difficulty
+export const getDifficultyBadgeClass = (difficulty) => {
+  switch (difficulty.toLowerCase()) {
+    case "easy":
+      return "badge-success";
+    case "medium":
+      return "badge-warning";
+    case "hard":
+      return "badge-error";
+    default:
+      return "badge-ghost";
+  }
+};
